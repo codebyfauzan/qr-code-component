d185f4560b9b73682d260c38418be9b2e0098a24
fauzan reza <fauzanreza23@gmail.com>
2024-09-14 14:50:58 +0700

    done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "slate-900": "#1F314F",
+        "slate-500": "#68778D",
+        "slate-300": "#D5E1EF",
+      },
+    },
+    fontFamily: {
+      Outfit: ["Outfit, sans-serif"],
+    },
+  },
+  plugins: [],
+};
